57a2abe6608064645230982b48e371519eb96d50
MuhammadHaris55 <hkhn567@gmail.com>
2021-08-05 03:00:11 +0500

    react js basic | jsx | props | component | state | fetching data from rest api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+
+//FUNCTIONAL COMPONENT 
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Coders Never Quit</h1>
+//     </div>
+//   );
+// }
+
+//? WAYS TO MAKE A FUNCTIONAL COMPONENT
+// function example(){ return( <div></div> )}
+// const example = () => { return( <div></div> ) }
+
+
+//!   PROPS CONCEPT ------------------------------------- START --------------------------------------
+//?   functional component  -----------------------------------------------
+const Coder = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{ props.name }</h1>
+      <h2>{ props.status }</h2>
+      <h3>{ props.children }</h3>
     </div>
-  );
+  )
+}
+
+//* CONVERTING ABOVE FUNCTIONAL COMPONENT IN CLASS BASED COMPONENT
+// we don't need to pass porps in class based component ..we only have to use this. when using props variables
+//?   class based compnent  ----------------------------------------------------
+// class Coder extends React.Component{
+//   render() {
+//     return (
+//       <div>
+//         <h1>{ this.props.name }</h1>
+//         <h2>{ this.props.status }</h2>
+//       </div>
+//     )
+//   }
+// }
+//CLASS BASE COMPNENT
+
+//! PROPS CONCEPT ------------------------------------- ENDS --------------------------------------
+
+
+
+class App extends React.Component{
+
+  state = {
+    title: "Haris"
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(res2 => console.log(res2))
+  }
+
+//  changeName = (name) => {
+    changeName(name) {
+    this.setState({
+      // title: "Bilal"
+      title: name
+    })
+  }
+
+  render() {
+
+    // const arr = ['eat', 'sleep', 'code'];
+    // const data = arr.map(item => {
+    //   return <h2>{ item }</h2>
+    // })
+
+    return(
+      <div className="App">
+      
+{/* JSX SYNTAX EXAMPLE ...JSX is basically a mixture of javascript and html
+      <h1>Coders Never Quit</h1>
+      <h2>{Math.random()}</h2> */}
+
+{/* ARRAY MAPPING ....USING DYNAMIC DATA
+      { data } */}
+
+{/* REUSEABILITY OF COMPONENT
+      <Coder />
+      <Coder /> */}
+      
+{/* PROPS CONCEPT
+      <Coder name="Haris" status="Coder"/>
+      <Coder name="Hummam" status="SQA"> Resigning </Coder>
+      <Coder name="MD" status="Laravel"/>
+      <Coder name="Bilal" status="FIFA"/> */}
+    
+{/* STATE CONCEPT */}
+    <h1>{ this.state.title }</h1>
+    <button onClick={()=> { this.changeName("Muqtada") }}>Change Name</button>
+
+    {/* <button onClick={this.changeName.bind(this, "Muqtada") }>Change Name by arrow function</button> */}    
+    
+    </div>
+    );
+  }
 }
 
 export default App;
